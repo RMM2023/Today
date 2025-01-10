287217591d83aa1dad1d7da7152d57030e66a68f
jimma <rumuzi1991@gmail.com>
2025-01-10 18:48:22 +0300

    ui cinema fragment

--- app/src/main/java/com/portfoliormm/today/presentation/ui/fragments/cinema/CinemaAdapter.kt
+++ app/src/main/java/com/portfoliormm/today/presentation/ui/fragments/cinema/CinemaAdapter.kt
@@ -4,9 +4,10 @@ import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.portfoliormm.today.R
+import com.portfoliormm.today.domain.model.FilterCinemaData
 
 class CinemaAdapter(var list : MutableList<FilmItemData>) : RecyclerView.Adapter<CinemaViewHolder>() {
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CinemaViewHolder {
+    override fun onCreateViewHolder(parent : ViewGroup, viewType : Int) : CinemaViewHolder {
         val view = LayoutInflater.from(parent.context)
             .inflate(R.layout.list_cinema, parent, false)
 
@@ -24,6 +25,19 @@ class CinemaAdapter(var list : MutableList<FilmItemData>) : RecyclerView.Adapter
     fun updateAdapter(newlist : MutableList<FilmItemData>){
         list = newlist
         notifyDataSetChanged()
+    }
+
+    fun updateAdapter(filter : FilterCinemaData){//дполнение функции адаптер чтобы могла принимать фильтр
+        val newlist : MutableList<FilmItemData> = mutableListOf()//создаем временный лист
+        filter.items.forEach{//проходимся по списку фильмов из фильтра
+            val film = FilmItemData(//создаем экземпляр FilmItemData для каждого из фильмов фильтра
+                name = it.nameRu,//переводим переменную из фильтра в новую
+                poster = it.posterUrl,//переводим переменную из фильтра в новую
+                favourite = false//переводим переменную из фильтра в новую
+            )
+            newlist.add(film)//добовляем полученный экземпляр к временному списку
+        }
+        updateAdapter(newlist)//вызываем базовую функцию передавая ей временный список
     }
 
     fun clearAdapter(){
--- app/src/main/java/com/portfoliormm/today/presentation/ui/fragments/cinema/MyCinemaFragment.kt
+++ app/src/main/java/com/portfoliormm/today/presentation/ui/fragments/cinema/MyCinemaFragment.kt
@@ -5,12 +5,15 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.portfoliormm.today.databinding.FragmentMyCinemaBinding
 import com.portfoliormm.today.presentation.viewmodel.CinemaViewModel
 import org.koin.androidx.viewmodel.ext.android.viewModel
 
 class MyCinemaFragment : Fragment() {
 
+    private lateinit var adapterRecomended :CinemaAdapter // объявляеются переменные адаптеров
+    private lateinit var adapterNew : CinemaAdapter // как вверху
     private lateinit var binding: FragmentMyCinemaBinding
     private val cinemaViewModel : CinemaViewModel by viewModel()
 
@@ -29,18 +32,33 @@ class MyCinemaFragment : Fragment() {
         observeViewModel()
     }
 
+    override fun onResume() {
+        super.onResume()
+        cinemaViewModel.searchRecomendedFilms()
+    }
+
     fun setupListeners(){
 
     //здесь будет кликабельность кнопок
     }
 
     fun setupAdapters(){
-        //здесь будет инициализация recyclerview
+        adapterRecomended = CinemaAdapter(mutableListOf()) //инициализировали адаптер
+        binding.recRecommendationsCinema.apply {
+            adapter = adapterRecomended
+            layoutManager = LinearLayoutManager(requireContext()) //с запросом контекста у родительского Activity
+        }
+        adapterNew = CinemaAdapter(mutableListOf())
+        binding.recPremierCinema.apply {
+            adapter = adapterNew
+            layoutManager = LinearLayoutManager(requireContext())
+        }
+    //здесь будет инициализация recyclerview
     }
 
     fun observeViewModel(){
-        cinemaViewModel.recomendedFilms.observe(viewLifecycleOwner){filmContainer->
-            //здесь будет заполнение recyclerview
+        cinemaViewModel.recomendedFilms.observe(viewLifecycleOwner){filter->
+            adapterRecomended.updateAdapter(filter)//здесь будет заполнение recyclerview
         }
     }
 }
